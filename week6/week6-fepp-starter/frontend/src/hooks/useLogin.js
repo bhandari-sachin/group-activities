91c77db0d826208e380d386885278b5f9a3a1267
sachin bhandari <bhandari.sachin.fi@gmail.com>
2025-11-30 00:56:20 +0200

    [iter3] Implement useLogin hook

---
+++ week6/week6-fepp-starter/frontend/src/hooks/useLogin.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export const useLogin = () => {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const login = async (email, password) => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch("/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const json = await response.json();
+      if (!response.ok) {
+        setIsLoading(false);
+        setError(json.error || "Login failed");
+        return null;
+      }
+      localStorage.setItem("user", JSON.stringify(json));
+      setIsLoading(false);
+      return json;
+    } catch (error) {
+      setIsLoading(false);
+      setError("An error occurred during login");
+      return null;
+    }
+  };
+  return { login, isLoading, error };
+};
--- week6/week6-fepp-starter/frontend/src/pages/LoginComponent.jsx
+++ week6/week6-fepp-starter/frontend/src/pages/LoginComponent.jsx
@@ -1,32 +1,19 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useLogin } from "../hooks/useLogin";
 
 const LoginComponent = ({ setIsAuthenticated }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
+  const { login, isLoading, error } = useLogin();
 
   const handleLogin = async () => {
-    try {
-      const response = await fetch("/api/users/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, password }),
-      });
+    const user = await login(email, password);
 
-      if (response.ok) {
-        const user = await response.json();
-        localStorage.setItem("user", JSON.stringify(user));
-        console.log("User logged in successfully!");
-        setIsAuthenticated(true);
-        navigate("/");
-      } else {
-        console.error("Login failed");
-      }
-    } catch (error) {
-      console.error("Error during login:", error);
+    if (user) {
+      setIsAuthenticated(true);
+      navigate("/");
     }
   };
 
@@ -51,7 +38,10 @@ const LoginComponent = ({ setIsAuthenticated }) => {
         />
       </label>
       <br />
-      <button onClick={handleLogin}>Log In</button>
+      <button onClick={handleLogin} disabled={isLoading}>
+        {isLoading ? "Logging in..." : "Log in"}
+      </button>
+      {error && <p style={{ color: "red" }}>{error}</p>}
     </div>
   );
 };
