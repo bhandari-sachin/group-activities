5aa4a8fe76673d0837bdd6eac0c3c54071099a4f
sachin bhandari <bhandari.sachin.fi@gmail.com>
2025-11-30 02:11:17 +0200

    [iter9] Use useField in Login

--- week6/week6-fepp-starter/frontend/src/pages/LoginComponent.jsx
+++ week6/week6-fepp-starter/frontend/src/pages/LoginComponent.jsx
@@ -1,18 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useLogin } from "../hooks/useLogin";
+import useField from "../hooks/useField";
 
 const LoginComponent = ({ setIsAuthenticated }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
+  // useField for inputs
+  const email = useField("text");
+  const password = useField("password");
+
   const { login, isLoading, error } = useLogin();
+  const navigate = useNavigate();
 
   const handleLogin = async () => {
-    const user = await login(email, password);
+    const user = await login(email.value, password.value);
 
     if (user) {
       setIsAuthenticated(true);
+
+      // reset fields after successful login
+      email.reset();
+      password.reset();
+
       navigate("/");
     }
   };
@@ -20,27 +28,23 @@ const LoginComponent = ({ setIsAuthenticated }) => {
   return (
     <div className="form-container">
       <h2>Login</h2>
+
       <label>
-        email:
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+        Email:
+        <input {...email} />
       </label>
       <br />
+
       <label>
         Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+        <input {...password} />
       </label>
       <br />
+
       <button onClick={handleLogin} disabled={isLoading}>
         {isLoading ? "Logging in..." : "Log in"}
       </button>
+
       {error && <p style={{ color: "red" }}>{error}</p>}
     </div>
   );
